1b790297afa2fe498f8c46f79073cc9491489fbd
Phuc Nguyen <nhphuc@starboardasia.com>
2020-11-23 18:09:24 +0700

    update page kurashi

---
+++ ssi/js/special.js
@@ -0,0 +1,37 @@
+
+jQuery(document).ready(function() {
+    // jQuery("#viewport").mapbox();
+    jQuery(".map-control a").click(function() {
+		var viewport = $("#viewport");
+		if(this.className == "back") {
+			viewport.mapbox(this.className, 2);
+		}
+		else {
+			viewport.mapbox(this.className);
+		}
+		return false;
+	});
+
+	jQuery(".fields_inner .category").click(function() {
+		var id = jQuery(this).attr("data-id");
+        jQuery( "." + id ).toggle();
+        if (jQuery(this).parent().hasClass("active")){
+        	jQuery(this).parent().removeClass("active");
+        }
+        else{
+        	jQuery(this).parent().addClass("active");
+        }
+	});
+
+});
+
+$(document).ready(function(){
+    $(".inline").colorbox({inline:true, width:"750px", height:"400px"});
+});
+
+// $(function(){
+//      $(".tooltip a").hover(function() {
+//         $(this).next("span").animate({opacity: "show"}, "slow");}, function() {
+//                $(this).next("span").animate({opacity: "hide"}, "fast");
+//      });
+// });
---
+++ ssi/kurashi.php
@@ -0,0 +1,375 @@
+<!DOCTYPE html>
+<!--[if IE 6]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7 eq-ie6" lang="ja"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8 eq-ie7" lang="ja"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9 eq-ie8" lang="ja"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="ja">
+<!--<![endif]-->
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"
+        content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+    <meta charset="utf-8">
+    <title>暮らしを支えるSSI｜株式会社ソフトウエア・サイエンス（SSI）</title>
+    <!--seo-->
+    <meta name="Keywords" content="SSI,Software Science Inc.,システム開発,ITコンサル,池袋" />
+    <meta name="description"
+        content="ソフトウエア・サイエンス（SSI）は1972年11月創業。純粋持株会社SRAHDの中核として、中堅企業向けソリューションビジネスを展開するとともに自社パッケージの販売にも注力しています。" />
+    <meta name="robots" content="index,follow" />
+    <!--OGP-->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="http://www.ssi.co.jp/" />
+    <meta property="og:title" content="会社案内｜株式会社ソフトウエア・サイエンス（SSI）" />
+
+    <meta property="og:site_name" content="SSI" />
+    <meta property="og:description"
+        content="ソフトウエア・サイエンス（SSI）は1972年11月創業。純粋持株会社SRAHDの中核として、中堅企業向けソリューションビジネスを展開するとともに自社パッケージの販売にも注力しています。" />
+
+    <!--setting-->
+    <link rel="shortcut icon" href="images/favicon.ico" />
+    <meta name="format-detection" content="telephone=no">
+    <!--css-->
+    <link rel="stylesheet" href="slick/slick.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/nav.css" />
+    <link rel="stylesheet" href="css/kurashi.css" />
+	<?php include 'partials/analytics.php'; ?>
+</head>
+
+<body>
+    <!-- Header -->
+    <?php include 'partials/header.php'; ?>
+    <!-- sub visual -->
+    <div class="sub-visual">
+        <h2 class="sub-visual-txt">暮らしを支えるSSI</h2>
+    </div>
+
+    <div class="kurashi">
+       <!-- Process -->
+        <section class="process">
+            <div class="container kurashi-content">
+                <div id="viewport">
+                    <div>
+                        <img src="images/kurashi/map_img.png" alt="" />
+                    </div>  
+            
+                    <div class="mark01 red"><p class="tooltip"><a href="#inline_content01" class="inline"><img src="images/kurashi/building.png" width="60" height="73" alt="" /></a></p></div>
+                    <div class="mark02 blue"><p class="tooltip"><a href="#inline_content02" class="inline"><img src="images/kurashi/education001.png" width="60" height="74" alt="" /></a></p></div>
+                    <div class="mark03 blue"><p class="tooltip"><a href="#inline_content03" class="inline"><img src="images/kurashi/education002.png" width="60" height="74" alt="" /></a></p></div>
+                    <div class="mark04 blue"><p class="tooltip"><a href="#inline_content04" class="inline"><img src="images/kurashi/education003.png" width="60" height="73" alt="" /></a></p></div>
+                    <div class="mark05 yellow"><p class="tooltip"><a href="#inline_content05" class="inline"><img src="images/kurashi/financial.png" width="60" height="73" alt="" /></a></p></div>
+                    <div class="mark06 red"><p class="tooltip"><a href="#inline_content06" class="inline"><img src="images/kurashi/medical.png" width="60" height="74" alt="" /></a></p></div>
+                    <div class="mark07 yellow"><p class="tooltip"><a href="#inline_content07" class="inline"><img src="images/kurashi/merchandise001.png" width="60" height="73" alt="" /></a></p></div>
+                    <div class="mark08 yellow"><p class="tooltip"><a href="#inline_content08" class="inline"><img src="images/kurashi/merchandise002.png" width="60" height="74" alt="" /></a></p></div>
+                    <div class="mark09 red"><p class="tooltip"><a href="#inline_content09" class="inline"><img src="images/kurashi/move.png" width="60" height="74" alt="" /></a></p></div>
+                    <div class="mark10 red"><p class="tooltip"><a href="#inline_content10" class="inline"><img src="images/kurashi/train.png" width="60" height="74" alt="" /></a></p></div>
+
+                    <div class="info01 yellow"><img src="images/kurashi/title-1.png" alt="" /></div>
+                    <div class="info02 red"><img src="images/kurashi/title-2.png" alt="" /></div>
+                    <div class="info03 yellow"><img src="images/kurashi/title-3.png" alt="" /></div>
+                    <div class="info04 yellow"><img src="images/kurashi/title-4.png" alt="" /></div>
+                    <div class="info05 blue"><img src="images/kurashi/title-5.png" alt="" /></div>
+                    <div class="info06 blue"><img src="images/kurashi/title-6.png" alt="" /></div>
+                    <div class="info07 red"><img src="images/kurashi/title-7.png" alt="" /></div>
+                    <div class="info08 blue"><img src="images/kurashi/title-8.png" alt="" /></div>
+                    <div class="info09 red"><img src="images/kurashi/title-9.png" alt="" /></div>
+                    <div class="info10 red"><img src="images/kurashi/title-10.png" alt="" /></div>
+                    <div class="wrap_fields">
+                        <div class="field_item">
+                            <div class="fields_inner">
+                                <div class="field_title field_title_1">
+                                    <a href="javascript:void(0);" data-id="red" class="category system01"><span>製造IT分野</span></a>
+                                </div>
+                                <div class="field_content">
+                                    日本のものづくりを支える製造業を支援
+                                </div>
+                            </div>
+                        </div>
+                        <div class="field_item">
+                            <div class="fields_inner">
+                                <div class="field_title field_title_2">
+                                    <a href="javascript:void(0);" data-id="yellow" class="category system02"><span>流通・金融分野</span></a>
+                                </div>
+                                <div class="field_content">
+                                    日々の暮らしを今よりもっと快適に
+                                </div>
+                            </div>
+                        </div>
+                        <div class="field_item">
+                            <div class="fields_inner">
+                                <div class="field_title field_title_3">
+                                    <a href="javascript:void(0);" data-id="blue" class="category system03"><span>教育分野</span></a>
+                                </div>
+                                <div class="field_content">
+                                    日々進化する教育業界を担う
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+        </section>
+
+        <div style="display:none">
+            <div id="inline_content01" class="box_inn pt90">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <img src="images/kurashi/009.png" width="334px" height="100px" alt="" />  
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <p>建物内のあらゆる設備機器と連動し、設備の使用時間や電力使用量を監視・制御。メンテナンスや管理業務の効率化に貢献。</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 logo">
+                                <img src="images/kurashi/IPROS8838307424943006954.png" alt="" />  
+                            </div>
+                        </div>    
+                    </div>
+                    <div class="col-lg-6 image-item">
+                        <img src="images/kurashi/building-1.png" alt="" /> 
+                    </div>
+                </div>
+            </div>
+            <div id="inline_content02" class="box_inn pt90">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <img src="images/kurashi/006.png" alt="" />  
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <p>学習履歴の分析により、個々の子どもに応じたきめ細やかな指導が実現可能となり、エビデンスベースト基づく教育を実現。</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 logo">
+                                <img src="images/kurashi/sgs.png" alt="" />  
+                            </div>
+                        </div>    
+                    </div>
+                    <div class="col-lg-6 image-item">
+                        <img src="images/kurashi/school003.png" alt="" /> 
+                    </div>
+                </div>
+            </div>
+            <div id="inline_content03" class="box_inn">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <img src="images/kurashi/005.png" alt="" />  
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <p>
+                                    小、中、高等学校にて使用されるタブレット、パソコン、ネットワークなどの学習環境の設備、授業支援をはじめ、WEB出願や入試関連システムの構築に至るまで支援。
+                                </p>    
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 logo">
+                                <img src="images/kurashi/sgs.png" alt="" />  
+                            </div>
+                        </div>    
+                    </div>
+                    <div class="col-lg-6 image-item">
+                        <img src="images/kurashi/school002.png" alt="" /> 
+                    </div>
+                </div>
+            </div>
+            <div id="inline_content04" class="box_inn">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <img src="images/kurashi/004.png" width="334px" height="95px" alt="" />  
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <p>AIを活用した学習支援機能をはじめ、進歩管理、学習履歴管理、学校内外で発表する資料の生成など、教員・生徒の探究学習を支援。</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 logo">
+                                <img src="images/kurashi/sgs.png" alt="" />  
+                            </div>
+                        </div>    
+                    </div>
+                    <div class="col-lg-6 image-item">
+                        <img src="images/kurashi/school001.png" alt="" /> 
+                    </div>
+                </div>
+            </div>
+            <div id="inline_content05" class="box_inn">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <img src="images/kurashi/003.png" width="354px" height="96px" alt="" />  
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <p>銀行の取引をインターネット上で実現。銀行の窓口やATMに行かなくても、振込や支払、残高照会が可能となることで、利用者の利便性が大幅に向上。</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 logo">
+                                <img src="images/kurashi/IPROS8838307424943006954.png" alt="" />  
+                            </div>
+                        </div>    
+                    </div>
+                    <div class="col-lg-6 image-item">
+                        <img src="images/kurashi/bank.png" alt="" /> 
+                    </div>
+                </div>
+            </div>
+            <div id="inline_content06" class="box_inn pt90">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <img src="images/kurashi/007.png" width="333x" height="99px" alt="" />  
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <p>
+                        院内や薬局で管理する薬剤を画像解析技術で自動判別。薬剤の廃棄や返却の仕分作業を大幅に軽減。
+                                </p>    
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 logo">
+                                <img src="images/kurashi/IPROS8838307424943006954.png" alt="" />  
+                            </div>
+                        </div>    
+                    </div>
+                    <div class="col-lg-6 image-item">
+                        <img src="images/kurashi/hospital.png" alt="" /> 
+                    </div>
+                </div>               
+            </div>
+            <div id="inline_content07" class="box_inn">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <img src="images/kurashi/001.png" width="341px" height="99px" alt="" />  
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <p>顧客情報管理、ポイント管理サービスを提供。お客様サービスを拡大し、顧客囲い込み、売上拡大に貢献。</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 logo">
+                                <img src="images/kurashi/IPROS8838307424943006954.png" alt="" />  
+                            </div>
+                        </div>    
+                    </div>
+                    <div class="col-lg-6 image-item">
+                        <img src="images/kurashi/pharmacy.png" alt="" /> 
+                    </div>
+                </div>
+            </div>
+            <div id="inline_content08" class="box_inn">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <img src="images/kurashi/002.png" width="314px" height="100px" alt="" />  
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <p>業者販売車両の検索システム。お客様のニーズに合う車両をリアルタイムに検索し、見積書の作成を自動化。</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 logo">
+                                <img src="images/kurashi/IPROS8838307424943006954.png" alt="" />  
+                            </div>
+                        </div>    
+                    </div>
+                    <div class="col-lg-6 image-item">
+                        <img src="images/kurashi/shop.png" alt="" /> 
+                    </div>
+                </div>
+            </div>
+            <div id="inline_content09" class="box_inn">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <img src="images/kurashi/008.png" width="342px" height="97px" alt="" />  
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <p>物流センター内において、コンビニエンスストアで販売されている商品の店別「仕分け」の自動化を支援。</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 logo">
+                                <img src="images/kurashi/IPROS8838307424943006954.png" alt="" />  
+                            </div>
+                        </div>    
+                    </div>
+                    <div class="col-lg-6 image-item">
+                        <img src="images/kurashi/souko.png" alt="" /> 
+                    </div>
+                </div>
+            </div>
+            <div id="inline_content10" class="box_inn">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <img src="images/kurashi/010.png" width="314px" height="99px" alt="" />  
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <p>列車の運行計画作成、運行状況案内、運行実績の集計と把握などを行うことにより、安全で安定した運行を支援。</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 logo">
+                                <img src="images/kurashi/IPROS8838307424943006954.png" alt="" />  
+                            </div>
+                        </div>    
+                    </div>
+                    <div class="col-lg-6 image-item">
+                        <img src="images/kurashi/train002.png" alt="" /> 
+                    </div>
+                </div>
+            </div>           
+        </div>
+       
+        <!-- common contact -->
+        <?php include 'partials/common_contact.php'; ?>
+    </div>   
+    <?php include 'partials/footer.php'; ?>
+    <script type="text/javascript" src="/js/jquery-1.7.2.min.js"></script>
+    <script type="text/javascript" src="/js/mapbox.min.js"></script>
+    <script type="text/javascript" src="/js/special.js"></script>
+    <script type="text/javascript" src="/js/jquery.colorbox-min.js"></script>
+</body>
+
+</html>
